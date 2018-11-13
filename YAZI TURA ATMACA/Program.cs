7a6a3cdc4e9e8e4385dc38af4ece08914d43fac6
omrcelkl <44753206+omrcelkl@users.noreply.github.com>
2018-11-13 11:20:37 +0300

    Add files via upload

---
+++ YAZI TURA ATMACA/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace deneme_6
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Random r = new Random();
+            for (int i = 0 ; i <10 ; i++)
+            {
+                int yazıtura = r.Next(1,3);
+
+                if(yazıtura==1)
+                    Console.WriteLine("YAZI");
+                else 
+                    Console.WriteLine("TURA");
+
+
+            }    
+        }
+    }
+}
