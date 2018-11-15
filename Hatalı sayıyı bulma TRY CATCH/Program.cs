7c8a656668c5baf80b01c5c6b13e8c19d03af35f
omrcelkl <44753206+omrcelkl@users.noreply.github.com>
2018-11-15 10:44:04 +0300

    Add files via upload

---
+++ Hatalı sayıyı bulma TRY CATCH/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace DENEME_7
+{
+    class StudentPoll
+    {
+        static void Main()
+        {
+            int [] responses = {1,2,5,4,3,5,2,1,3,3,1,4,3,3,3,2,3,3,2,14};
+
+            var frequency = new int [6];
+
+            for ( var answer = 0 ; answer < responses.Length; ++answer)
+            {
+                try
+                {
+                    ++frequency[responses[answer]];
+
+                }
+
+                catch (IndexOutOfRangeException ex)
+                {
+                    Console.WriteLine(ex.Message);
+                    Console.WriteLine($"[{answer}]= {responses[answer]}\n");
+                }
+            }
+        }
+    }
+}
