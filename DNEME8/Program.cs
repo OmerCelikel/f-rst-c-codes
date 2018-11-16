8b103c234d52466f5946742ecfa6814cb584d476
omrcelkl <44753206+omrcelkl@users.noreply.github.com>
2018-11-16 11:47:29 +0300

    Add files via upload

---
+++ DNEME8/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+// 0 a 0 --- 1 e 1 --- 2 ye 2--- 3 e 3 --- toplamı
+
+namespace DNEME8
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+           int [,] a = {{1,2,3,4},
+                        {4,66,32,43},
+                        {32,43,66,54},
+                        {324, 497,548,032}};
+            int toplam = 0 ;
+            for (int satir = 0 ; satir < a.GetLength(0) ; satir++)
+            {
+                for(int sutun = 0; sutun < a.GetLength(1); sutun++)
+                {
+                    // satır sütüna eşit olduğunda topllama yapacağız
+                    if(satir==sutun)
+                    toplam = toplam + a[satir , sutun];    
+                }
+            }
+
+            Console.Write(toplam);
+        }
+    }
+}
