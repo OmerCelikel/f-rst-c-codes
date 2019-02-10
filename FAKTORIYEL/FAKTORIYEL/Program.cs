bf060040d8ebe2cf0a1e05a3b26e519d4e3a80b7
omrcelkl <44753206+omrcelkl@users.noreply.github.com>
2019-02-10 11:44:03 +0300

    Metot ile FAKTORIYEL

---
+++ FAKTORIYEL/FAKTORIYEL/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace FAKTORIYEL
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            for (long counter = 0; counter<=10; ++counter)
+            {
+                Console.WriteLine($"{counter}! = {Factorial(counter)}");
+            }
+            Console.ReadKey();
+        }
+
+        static long Factorial(long number)
+        {
+            if (number <= 1)
+            {
+                return 1;
+            }
+            else
+            {
+                return number * Factorial(number - 1);
+            }
+            
+        }
+            
+        
+    }
+}
